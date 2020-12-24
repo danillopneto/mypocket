f8bd0fea7f14320709fcd42c6601fe67d14cba53
danillopneto <daynilou@gmail.com>
2020-12-24 16:42:32 -0300

    Creating a simple service showing a message of success.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,8 +18,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { RedDirective } from './directives/red.directive';
 import { JumpingDirective } from './directives/jumping.directive';
 import { ForDirective } from './directives/for.directive';
-import { ProductCreateComponent } from './components/product-create/product-create.component'
-import { MatButtonModule } from '@angular/material/button'
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { MatButtonModule } from '@angular/material/button'
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
